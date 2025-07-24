a305dfd4a0b0e1f8549c442ae6d2e7fccc183a73
Filli2000 <filipinhoo@live.com>
2025-07-24 19:22:09 -0300

    Update B CartasSuperTrunfo.c
    atualização do codigo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,29 +2,20 @@
 
 int main(){
 
-    //Variaveis da Carta 01
-    char Estado1[20];
-    char Código1[3];
-    char Nome1[20];
+ //incio do jogo - introdução
+ printf ("  Bem-vindo ao Super Trunfo 2000!\n Insira as informações solicitadas no prompt com a cidade Brasileira de sua escolha abaixo:\n");
+
+     //Variaveis da Carta 01
+    char Estado1;
+    char Código1;
+    char Nome1;
     int População1;
     int Turisticos1;
     float Area1;
     float PIB1;
-    
-    //Variaveis da Carta 02
-    char Estado2[20];
-    char Código2[3];
-    char Nome2[20];
-    int População2;
-    int Turisticos2;
-    float Area2;
-    float PIB2;
-
-//incio do jogo - introdução
- printf ("  Bem-vindo ao Super Trunfo 2000!\n Insira as informações solicitadas no prompt com a cidade Brasileira de sua escolha abaixo:\n");
 
  //Primeira carta
-  printf ("Qual o Estado da cidade escolhida? (Use _ em vez de Espaços)\n");
+  printf ("Qual o Estado da cidade escolhida? (Sem espaços)\n");
   scanf ("%s", &Estado1);
 
   printf ("Crie um código para a carta:\n");
@@ -48,15 +39,63 @@ int main(){
  //Primeira carta - resultado
  printf ("\n Certo! Aqui estão as informações de sua primeira carta:\n");
  
- printf ("Estado: %s\n", Estado1);  //falha
+ printf ("Estado: %s\n", Estado1);
  printf ("Cidade: %s\n", Nome1);
- printf ("Código: %s\n", Código1);  //falha
+ printf ("Código: %s\n", Código1);
  printf ("População: %d\n", População1);
  printf ("Atrações Turísticas: %d\n", Turisticos1);
- printf ("Área: %0.4f\n", Area1);
+ printf ("Área: %f\n", Area1);
  printf ("Produto Interno Bruto (PIB): %f\n", PIB1);
 
+    //Variaveis da Carta 02
+    char Estado2;
+    char Código2;
+    char Nome2;
+    int População2;
+    int Turisticos2;
+    float Area2;
+    float PIB2;
+
+ //Segunda carta
+ printf ("\n Agora vamos repetir o processo para a Segunda Carta!\n");
+
+  printf ("Qual o Estado da cidade escolhida? (Sem espaços)\n");
+  scanf ("%s", &Estado2);
+
+  printf ("Crie um código para a carta:\n");
+  scanf ("%s", &Código2);
+
+  printf ("Nome da cidade:\n");
+  scanf ("%s", &Nome2);
+ 
+  printf ("População:\n");
+  scanf ("%d", &População2);
+
+  printf ("Número de atrações turísticas:\n");
+  scanf ("%d", &Turisticos2);
+
+  printf ("Área em KM²:\n");
+  scanf ("%f", &Area2);
+
+  printf ("PIB:\n");
+  scanf ("%f", &PIB2);
+
+  //Segunda carta - resultado
+ printf ("\n Certo! Aqui estão as informações de sua segunda carta:\n");
+ 
+ printf ("Estado: %s\n", Estado2);
+ printf ("Cidade: %s\n", Nome2);
+ printf ("Código: %s\n", Código2);
+ printf ("População: %d\n", População2);
+ printf ("Atrações Turísticas: %d\n", Turisticos2);
+ printf ("Área: %f\n", Area2);
+ printf ("Produto Interno Bruto (PIB): %f\n", PIB2);
+
+ //Fim de jogo
+ printf ("Obrigado por jogar!");
 
  return 0;
+
+
 
 }
